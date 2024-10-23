4b292ca51e19ee84adde6848364599e8b9b25a95
abelpriem <abelpriem94@hotmail.com>
2024-10-23 14:18:14 +0200

    añadido a la rama el Backpack repository & services

--- src/main/java/org/f5games/aiden_game/repository/BackpackRepository.java
+++ src/main/java/org/f5games/aiden_game/repository/BackpackRepository.java
@@ -3,6 +3,12 @@ package org.f5games.aiden_game.repository;
 import org.f5games.aiden_game.models.Backpack;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BackpackRepository extends JpaRepository<Backpack, Long> {  
+public interface BackpackRepository extends JpaRepository<Backpack, Long> {
+
+    void insertOne(Backpack backpack);
+
+    void updateOne(Long objectId, Backpack backpack);
+
+    void deleteOne(Long objectId);
 
 }
--- src/main/java/org/f5games/aiden_game/services/BackpackService.java
+++ src/main/java/org/f5games/aiden_game/services/BackpackService.java
@@ -1,5 +1,7 @@
 package org.f5games.aiden_game.services;
+
 import java.util.List;
+
 import org.f5games.aiden_game.models.Backpack;
 import org.f5games.aiden_game.repository.BackpackRepository;
 
@@ -11,26 +13,19 @@ public class BackpackService {
         this.backpackRepository = backpackRepository;
     }
 
-    public List<Backpack> getAll(){
-
+    public List<Backpack> getAll() {
         return backpackRepository.findAll();
-        
     }
 
     public void insertOne(Backpack backpack) {
-
         backpackRepository.insertOne(backpack);
-
     }
 
     public void updateOne(Long objectId, Backpack backpack) {
-
-        backpackRepository.updateOne(objectId,backpack);
-
+        backpackRepository.updateOne(objectId, backpack);
     }
 
     public void deleteOne(Long objectId) {
-
         backpackRepository.deleteOne(objectId);
     }
 

3e65974d92b531e17e4628d500491bba22e3201a
SusanaArtime <susartime@gmail.com>
2024-10-23 14:11:13 +0200

    subo archivo BackpackService.java

--- src/main/java/org/f5games/aiden_game/services/BackpackService.java
+++ src/main/java/org/f5games/aiden_game/services/BackpackService.java
@@ -1,25 +1,37 @@
 package org.f5games.aiden_game.services;
-
 import java.util.List;
-
 import org.f5games.aiden_game.models.Backpack;
+import org.f5games.aiden_game.repository.BackpackRepository;
 
 public class BackpackService {
 
-    public List<Backpack> getAll() {
-        return null;
+    private final BackpackRepository backpackRepository;
+
+    public BackpackService(BackpackRepository backpackRepository) {
+        this.backpackRepository = backpackRepository;
+    }
+
+    public List<Backpack> getAll(){
+
+        return backpackRepository.findAll();
+        
     }
 
     public void insertOne(Backpack backpack) {
+
+        backpackRepository.insertOne(backpack);
 
     }
 
     public void updateOne(Long objectId, Backpack backpack) {
 
+        backpackRepository.updateOne(objectId,backpack);
+
     }
 
     public void deleteOne(Long objectId) {
 
+        backpackRepository.deleteOne(objectId);
     }
 
 }

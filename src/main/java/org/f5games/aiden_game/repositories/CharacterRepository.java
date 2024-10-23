58bb5a6820cff81ae17ffa8156d83652d57ec7a7
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-10-23 11:52:27 +0200

    estructura de carpetas en local

---
+++ src/main/java/org/f5games/aiden_game/repositories/CharacterRepository.java
@@ -0,0 +1,7 @@
+package org.f5games.aiden_game.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CharacterRepository extends JpaRepository<Character, Integer>{
+    
+}

15413703788fbbd2a86155d662aba41034edd8dd
SusanaArtime <susartime@gmail.com>
2024-10-25 14:06:36 +0200

    subo cambios de GameObject y Backpack a la rama 31

--- src/main/java/org/f5games/aiden_game/models/Backpack.java
+++ src/main/java/org/f5games/aiden_game/models/Backpack.java
@@ -1,12 +1,5 @@
 package org.f5games.aiden_game.models;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,4 +19,5 @@ public class Backpack {
     @ManyToOne
     @JoinColumn(name = "game_object_id")
     private GameObject gameObject;
+    
 }

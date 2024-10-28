f3bda78d4526bba1e1ceabc6514283b4d2cbd373
SusanaArtime <susartime@gmail.com>
2024-10-28 14:47:44 +0100

    subo clase ghost en models

---
+++ src/main/java/org/f5games/aiden_game/models/Ghost.java
@@ -0,0 +1,44 @@
+package org.f5games.aiden_game.models;
+import java.util.Random;
+
+public class Ghost extends Character{
+
+    private boolean skill1;
+
+    
+    public Ghost(Long id, String name, Integer strength, Integer health, boolean skill1) {
+        super(id, name, strength, health);
+        this.skill1 = skill1;
+    }
+
+    public Ghost(){
+
+    }
+
+    public Ghost(boolean skill1) {
+        this.skill1 = skill1;
+    }
+
+    public boolean isSkill1() {
+        return skill1;
+    }
+
+    public void setSkill1(boolean skill1) {
+        this.skill1 = skill1;
+    }
+
+    public boolean invisibility(boolean skill1){
+
+        Random rand = new Random();
+        int randomInvisibility = rand.nextInt(100) + 1; 
+        if(randomInvisibility < 30){
+
+            return true;
+        }
+
+        return false;
+
+    }
+
+    
+}

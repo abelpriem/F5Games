83e3dab8a1d9f6c67f78a6d2dcbafda374f1c179
Juan Camilo Garcia Cuevas <juanca.cuevas.05@outlook.com>
2024-10-29 09:10:04 +0100

    /character endpoint created entirely

--- src/main/java/org/f5games/aiden_game/controllers/CharacterController.java
+++ src/main/java/org/f5games/aiden_game/controllers/CharacterController.java
@@ -1,5 +1,30 @@
 package org.f5games.aiden_game.controllers;
 
-public class CharacterController {
+import java.util.List;
 
+import org.f5games.aiden_game.models.Character;
+import org.f5games.aiden_game.services.CharacterService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class CharacterController {
+    private CharacterService characterservice;
+
+    public CharacterController (CharacterService characterService) {
+        this.characterservice = characterService;
+    }
+    @GetMapping(path = "/character")
+    public List<Character> getCharacter(@RequestParam String param) {
+        return characterservice.getAll();
+    }
+    @PutMapping(path = "/character{id}")
+    public void updateCharacter(@PathVariable Long id, @RequestBody Character character) {
+        characterservice.updateOne(character);
+    }
 }
--- src/main/java/org/f5games/aiden_game/services/CharacterService.java
+++ src/main/java/org/f5games/aiden_game/services/CharacterService.java
@@ -1,5 +1,29 @@
 package org.f5games.aiden_game.services;
 
+import java.util.List;
+
+import org.f5games.aiden_game.models.Character;
+import org.f5games.aiden_game.repository.CharacterRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CharacterService {
 
+    private final CharacterRepository characterRepository;
+
+    public CharacterService (CharacterRepository character_repository) {
+        this.characterRepository = character_repository;
+    }
+
+    public List<Character> getAll() {
+        return characterRepository.findAll();
+    }
+
+    public void updateOne(Character character) {
+        if (characterRepository.existsById(character.getId())) {
+            characterRepository.save(character);
+        } else {
+            throw new IllegalArgumentException("character identifier" + character.getId() + "doesn't exist");
+        }
+    }
 }

da3d3bfc44394299fa3911a024e140f4c3e8ebbf
SusanaArtime <susartime@gmail.com>
2024-11-07 14:39:35 +0100

    subo modificación de test de models

--- src/test/java/org/f5games/aiden_game/models/BackpackTest.java
+++ src/test/java/org/f5games/aiden_game/models/BackpackTest.java
@@ -1,27 +1,17 @@
 package org.f5games.aiden_game.models;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import java.lang.reflect.Field;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BackpackTest {
 
-// INSERT INTO game_objects (id,name,description,special) VALUES (default,'Espada encantada','Aumenta el daño del héroe',false);
-    private Backpack backpack;
-
-    @BeforeEach
-    void setUp() {
-       // backpack = new Backpack(1L, GameObject.builder().description("mock description"));
-    }
-
     @Test
-    public void blablab() {
-
-
-    }
-
-
-
+        void setup(){
+    
+            Backpack backpack= new Backpack();
+        }
 }
+
+    
+
+
+
+
--- src/test/java/org/f5games/aiden_game/services/CharacterServiceTest.java
+++ src/test/java/org/f5games/aiden_game/services/CharacterServiceTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.f5games.aiden_game.models.Backpack;
 import org.f5games.aiden_game.models.Character;
 import org.f5games.aiden_game.repository.CharacterRepository;
 import org.junit.jupiter.api.BeforeEach;

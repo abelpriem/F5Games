a466c26604a4209c72251930c6ae2b6586fd55fd
Acacia-Sanchez-Pastur <sanchezpastur@gmail.com>
2024-11-07 14:14:11 +0100

    updated ascii with last colors and spaces

--- src/main/java/org/f5games/aiden_game/assets/ascii/Skeletons.java
+++ src/main/java/org/f5games/aiden_game/assets/ascii/Skeletons.java
@@ -3,24 +3,17 @@ package org.f5games.aiden_game.assets.ascii;
 public class Skeletons {
     public static void ascii(String[] args) {
 
-        String color1 = "\033[96m"; // cian
-        String color1b = "\033[1;96m"; // cian bold
-        String color2 = "\033[35m"; // magenta
-        String color3 = "\033[94m"; // azul
-        String color4 = "\033[91m"; // rojo
-        String color5 = "\033[92m"; // verde
-        String color6 = "\033[97m"; // blanco
-        String color7 = "\033[93m"; // amarillo
-        String color10 = "\033[38;5;206;48;5;57m";
-        String reset = "\033[0m"; // blanco
+        String color1 = "\033[96m";  // cian
+        String color7 = "\033[93m";  // amarillo
+        String reset = "\033[0m";  // blanco
 
         System.out.println();
         System.out.println(color1 + "         - - - - - - - - - - - - - - - - - - - - - - - - -");
-        System.out.println(color7 + "                        Monstruo: Esqueletos");
+        System.out.println(color7 + "                           Jefe: Esqueletos");
         System.out.println(color1 + "         - - - - - - - - - - - - - - - - - - - - - - - - -");
         System.out.print("               .-.            .-.             .-.     \n" + //
-                "              (o.o)          (o.o)           (o.o)\n" + //
-                "               |=|            |=|             |=|\n" + //
+                "              (o.o).         (o.o).          (o.o)\n" + //
+                "               |=|.           |=|.            |=|\n" + //
                 "              __|__.         __|__           __|__\n" + //
                 "            //.=|=.\\\\.     //.=|=.\\\\.      //.=|=.\\\\\n" + //
                 "           // .=|=. \\\\.   // .=|=. \\\\.    // .=|=. \\\\\n" + //
--- src/main/java/org/f5games/aiden_game/assets/ascii/Title.java
+++ src/main/java/org/f5games/aiden_game/assets/ascii/Title.java
@@ -5,20 +5,12 @@ public class Title {
 
     public static void ascii(String[] args) {
 
-        String color1 = "\033[96m"; // cian
-        String color1b = "\033[1;96m"; // cian bold
-        String color2 = "\033[35m"; // magenta
-        String color3 = "\033[94m"; // azul
-        String color4 = "\033[91m"; // rojo
         String color5 = "\033[92m"; // verde
-        String color6 = "\033[97m"; // blanco
-        String color7 = "\033[93m"; // amarillo
-        String color10 = "\033[38;5;206;48;5;57m";
         String reset = "\033[0m"; // blanco
 
         System.out.println();
         System.out.println(color5 + "         - - - - - - - - - - - - - - - - - - - - - - -");
-        System.out.print(color5 + "           ________    _____      _____   ___________\n" + //
+        System.out.print("           ________    _____      _____   ___________\n" + //
                 "          /  _____/   /  _  \\    /     \\  \\_   _____/\n" + //
                 "         /   \\  ___  /  /_\\  \\  /  \\ /  \\  |    __)_ \n" + //
                 "         \\    \\_\\  \\/    |    \\/    Y    \\ |        \\\n" + //

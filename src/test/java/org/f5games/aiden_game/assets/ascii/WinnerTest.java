6c488045b6b700704519af8730bd192d4b831452
SusanaArtime <susartime@gmail.com>
2024-11-07 16:45:15 +0100

    limpio warnings de ascii

--- src/test/java/org/f5games/aiden_game/assets/ascii/WinnerTest.java
+++ src/test/java/org/f5games/aiden_game/assets/ascii/WinnerTest.java
@@ -5,19 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.f5games.aiden_game.assets.ascii.Winner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class WinnerTest {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
 
     @BeforeEach
     public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
+        System.setOut(new PrintStream(outputStreamCaptor));
     }
 
     @AfterEach
@@ -30,13 +29,12 @@ public class WinnerTest {
         // Llamar al método que imprime el arte ASCII
         Winner.ascii(null);
 
-        // Obtener la salida de la consola
-        String output = outContent.toString();
+        String output = outputStreamCaptor.toString();
 
-        // Verificar que la salida contiene partes específicas del arte ASCII
-        assertTrue(output.contains("______   _____________________ ._."));
-        assertTrue(output.contains("\\_   _____/\\______   \\| |"));
-        assertTrue(output.contains("\\__/\\  /  |___|\\____|__  /\\____|__  //_______  / |____|_  / __"));
-        assertTrue(output.contains("         \\/                \\/         \\/         \\/  \\/"));
+        // Verifica que el arte ASCII contiene las partes esperadas
+        assertTrue(output.contains("           __      __ .___  _______    _______   _____________________ ._.          "));
+        assertTrue(output.contains("          - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -          "));
+        assertTrue(output.contains("             \\__/\\  /  |___|\\____|__  /\\____|__  //_______  / |____|_  / __          "));
+        assertTrue(output.contains("                  \\/                \\/         \\/         \\/         \\/  \\/          "));
     }
 }

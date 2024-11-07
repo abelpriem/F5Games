380a9e6394ce5bc005be5767f28f9d0e0a56ae37
SusanaArtime <susartime@gmail.com>
2024-11-07 15:34:19 +0100

    limpio warnings del ascii de Game Over

--- src/main/java/org/f5games/aiden_game/assets/ascii/GameOver.java
+++ src/main/java/org/f5games/aiden_game/assets/ascii/GameOver.java
@@ -3,15 +3,8 @@ package org.f5games.aiden_game.assets.ascii;
 public class GameOver {
         public static void ascii(String[] args) {
 
-                String color1 = "\033[96m";
-                String color1b = "\033[1;96m";
-                String color2 = "\033[35m";
-                String color3 = "\033[94m";
-                String color4 = "\033[91m";
-                String color5 = "\033[92m";
-                String color6 = "\033[97m";
+                
                 String color7 = "\033[93m";
-                String color10 = "\033[38;5;206;48;5;57m";
                 String reset = "\033[0m";
 
                 System.out.println();

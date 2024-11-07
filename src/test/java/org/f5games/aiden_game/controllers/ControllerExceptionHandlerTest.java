2e48eea4e750b8e17a468cb0b1f94a0f5b1cfa78
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-11-07 14:30:58 +0100

    añadidos test de controllerExceptionHandler

--- src/test/java/org/f5games/aiden_game/controllers/ControllerExceptionHandlerTest.java
+++ src/test/java/org/f5games/aiden_game/controllers/ControllerExceptionHandlerTest.java
@@ -3,6 +3,8 @@ package org.f5games.aiden_game.controllers;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.f5games.aiden_game.ControllerExceptionHandler;
+import org.f5games.aiden_game.exceptions.InvalidDataException;
+import org.f5games.aiden_game.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -20,32 +22,36 @@ public class ControllerExceptionHandlerTest {
 
     @Test
     void testHandleIllegalArgument() {
-        // Preparar la excepción
         IllegalArgumentException exception = new IllegalArgumentException("Test IllegalArgumentException");
-
-        // Invocar el método de manejo de excepción
         ResponseEntity<String> response = exceptionHandler.handleIllegalArgument(exception);
-
-        // Verificar que el estado de la respuesta es 400 Bad Request
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-
-        // Verificar que el cuerpo de la respuesta contiene el mensaje de la excepción
         assertEquals("Test IllegalArgumentException", response.getBody());
     }
 
     @Test
     void testHandleDataIntegrityViolation() {
-        // Preparar la excepción
         DataIntegrityViolationException exception = new DataIntegrityViolationException(
                 "Test DataIntegrityViolationException");
-
-        // Invocar el método de manejo de excepción
         ResponseEntity<String> response = exceptionHandler.handleDataIntegrityViolation(exception);
-
-        // Verificar que el estado de la respuesta es 400 Bad Request
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-
-        // Verificar que el cuerpo de la respuesta contiene el mensaje de la excepción
         assertEquals("Test DataIntegrityViolationException", response.getBody());
+    }
+
+    @Test
+    void testHandleResourceNotFound() {
+        ResourceNotFoundException exception = new ResourceNotFoundException(
+                "Test ResourceNotFoundException");
+        ResponseEntity<String> response = exceptionHandler.handleResourceNotFound(exception);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Test ResourceNotFoundException", response.getBody());
+    }
+
+    @Test
+    void testHandleInvalidData() {
+        InvalidDataException exception = new InvalidDataException(
+                "Test InvalidDataException");
+        ResponseEntity<String> response = exceptionHandler.handleInvalidData(exception);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Test InvalidDataException", response.getBody());
     }
 }
